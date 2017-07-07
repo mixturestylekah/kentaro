249295f92f0006d80121cd2676ec5a9145abb5eb
kentaro <mixture.style.k.a.h@gmail.com>
2017-07-07 14:48:43 +0900

    デプロイ環境の修正・２

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ role :web, %w{deploy@kentaro.run}
 role :db, %w{deploy@kentaro.run}
 
 set :ssh_options, {
-  keys: %w(/home/vagrant/.ssh/id_rsa)
-  forward_agent: false,
+  keys: %w(/home/vagrant/.ssh/id_rsa),
+  forward_agent: true,
   auth_methods: %w(publickey)
 }
